de5033dbf4ba3832d8e0276043e14b6ee537afb4
James <apisitamornktn@gmail.com>
2024-07-03 14:13:59 +0700

    feat:ALGORITHMS: Nested Loop (Exercise)

--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,14 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(n) {
+  let result = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      result += (i * j).toString().padStart(4, " ");
+    }
+    result += "\n";
+  }
+  return result;
 }
+
+const fiveTable = multiplicationTable(5);
+console.log(fiveTable);
